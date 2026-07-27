5d81f54aa4fcf55d150bf1fe5942dd7bdb801802
urdushahzaib111-ctrl <urdushahzaib111@gmail.com>
2026-07-27 19:41:56 +0500

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,38 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    
+    if ('data' in command && 'execute' in command) {
+        commands.push(command.data.toJSON());
+    } else if (Array.isArray(command.data)) {
+        for (const subCmd of command.data) {
+            commands.push(subCmd.toJSON());
+        }
+    }
+}
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        const data = await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
